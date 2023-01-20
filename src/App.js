08a67126a478a15850c4b624ee472c107d9a8b7c
Pumpkintitan <pumpkintitan11@gmail.com>
2023-01-19 20:04:32 -0700

    transition on change

--- src/App.js
+++ src/App.js
@@ -45,37 +45,37 @@ function App() {
             <h3>Stats</h3>
             <p>HP: {pokemon.stats[0].base_stat}
               <div className='wrapper'>
-                <div style={{ height: "10px", width: `${pokemon.stats[0].base_stat / 2.55}%`, backgroundColor: `hsl(${pokemon.stats[0].base_stat}, 100%, 50%)` }}>
+                <div className='bar' style={{width: `${pokemon.stats[0].base_stat / 2.55}%`, backgroundColor: `hsl(${pokemon.stats[0].base_stat}, 100%, 50%)` }}>
                 </div>
               </div>
             </p>
             <p>Attack: {pokemon.stats[1].base_stat}
             <div className='wrapper'>
-                <div style={{ height: "10px", width: `${pokemon.stats[1].base_stat / 1.81}%`, backgroundColor: `hsl(${pokemon.stats[1].base_stat}, 100%, 50%)` }}>
+                <div className='bar' style={{width: `${pokemon.stats[1].base_stat / 1.81}%`, backgroundColor: `hsl(${pokemon.stats[1].base_stat}, 100%, 50%)` }}>
                 </div>
               </div>
               </p>
             <p>Defense: {pokemon.stats[2].base_stat}
             <div className='wrapper'>
-                <div style={{ height: "10px", width: `${pokemon.stats[2].base_stat / 2.3}%`, backgroundColor: `hsl(${pokemon.stats[2].base_stat}, 100%, 50%)` }}>
+                <div className='bar' style={{width: `${pokemon.stats[2].base_stat / 2.3}%`, backgroundColor: `hsl(${pokemon.stats[2].base_stat}, 100%, 50%)` }}>
                 </div>
               </div>
               </p>
             <p>Special Attack: {pokemon.stats[3].base_stat}
             <div className='wrapper'>
-                <div style={{ height: "10px", width: `${pokemon.stats[3].base_stat / 1.8}%`, backgroundColor: `hsl(${pokemon.stats[3].base_stat}, 100%, 50%)` }}>
+                <div className='bar' style={{width: `${pokemon.stats[3].base_stat / 1.8}%`, backgroundColor: `hsl(${pokemon.stats[3].base_stat}, 100%, 50%)` }}>
                 </div>
               </div>
               </p>
             <p>Special Defense: {pokemon.stats[4].base_stat}
             <div className='wrapper'>
-                <div style={{ height: "10px", width: `${pokemon.stats[4].base_stat / 2.3}%`, backgroundColor: `hsl(${pokemon.stats[4].base_stat}, 100%, 50%)` }}>
+                <div className='bar' style={{width: `${pokemon.stats[4].base_stat / 2.3}%`, backgroundColor: `hsl(${pokemon.stats[4].base_stat}, 100%, 50%)` }}>
                 </div>
               </div>
               </p>
             <p>Speed: {pokemon.stats[5].base_stat}
             <div className='wrapper'>
-                <div style={{ height: "10px", width: `${pokemon.stats[5].base_stat / 2}%`, backgroundColor: `hsl(${pokemon.stats[5].base_stat}, 100%, 50%)` }}>
+                <div className='bar' style={{width: `${pokemon.stats[5].base_stat / 2}%`, backgroundColor: `hsl(${pokemon.stats[5].base_stat}, 100%, 50%)` }}>
                 </div>
               </div>
               </p>
